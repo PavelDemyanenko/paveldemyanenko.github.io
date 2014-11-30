d8d6296dbf19c8a102ee1fb2a5e7e7067a774d96
xeLL <conscious.suddens@gmail.com>
2014-12-01 00:52:11 +0300

    Update substitute config, update links, small style fix in menu.

--- substitute-config.js
+++ substitute-config.js
@@ -1,19 +1,19 @@
 module.exports = {
   analytics: 'UA-XXXXX-X',
 
-  description: '',
-  author: '',
-  keywords: '',
+  description: 'This site is used as code example for people who are interested in my skills.',
+  author: 'Pavel Demyanenko',
+  keywords: 'front end, ui, ux, angularjs, nodejs, javascript, html, css, portfolio',
 
-  'og:url': '',
-  'og:image': '',
-  'og:type': '',
-  'og:site_name': '',
+  'og:url': 'http://www.paveldemyanenko.io',
+  'og:image': 'https://avatars2.githubusercontent.com/u/506399?v=3&s=460',
+  'og:type': 'website',
+  'og:site_name': 'Pavel Demyanenk',
 
-  'twitter:title': '',
-  'twitter:url': '',
-  'twitter:image': '',
-  'twitter:card': '',
-  'twitter:site': '',
-  'twitter:creator': ''
+  'twitter:title': 'Pavel Demyanenko',
+  'twitter:url': 'http://www.paveldemyanenko.io',
+  'twitter:image': 'https://avatars2.githubusercontent.com/u/506399?v=3&s=460',
+  'twitter:card': 'summary',
+  'twitter:site': '@paveldemyanenko`',
+  'twitter:creator': '@paveldemyanenko'
 };
