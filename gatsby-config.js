bc08c5b347a31047ac14a6c6e2d9b69f9636dddc
PavelDemyanenko <mail@paveldemyanenko.com>
2018-06-03 17:07:30 +0300

    GatsbyJS blog init

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: path.resolve(__dirname, 'src/blog'),
+      },
+    },
+  ],
+}
---
+++ src/blog/first-blog-post.md
@@ -0,0 +1,8 @@
+---
+title: My first blog post
+date: "2018-04-20"
+---
+
+Do you enjoy Gabe the Dog? He is the immortal lead singer of Bork, a European band that does covers of popular pop songs from the 80s, 90s, and today.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/c--etqIJcow?ecver=1" frameborder="0" allowfullscreen></iframe
