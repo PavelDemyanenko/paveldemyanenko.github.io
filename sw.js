c8c3b0731934e5d4f2d11440541050fa7dc56616
Pavel Demianenko <Pavel.Demianenko@t-systems.com>
2021-01-24 22:22:10 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4f8d880d9f53ac59dff5.js"
+    "url": "webpack-runtime-a769c01f26cd7039a96b.js"
   },
   {
     "url": "styles.7abc6d405664f1aa25d5.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "116d373166e47d4ceee4bf38be16bc1c"
+    "revision": "1b4a8101bda1acd2047d596c7da44294"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "0910ce37a38ea9e76c8a2b087152e798"
+    "revision": "c011333b4690af67f3772e53fdf274a0"
   },
   {
     "url": "polyfill-f33cc994945b2249d5ee.js"
